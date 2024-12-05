391935f55d5f9fc67387b316c2f85a8c242f5082
kimkihoonthegoat <leeyc3016@gmail.com>
2024-12-05 15:35:31 +0900

    second assginment 반영

--- fileProcess.py
+++ fileProcess.py
@@ -1,15 +1,32 @@
 import pandas as pd
+import os
 class FileProcess:
-    def __init__(self,path,encoding='euc-kr'):
-        self.path = path
+    def __init__(self,encoding='euc-kr'):
+        self.path = None
         self.encoding = encoding
-        self.file = pd.read_csv(self.path, encoding=self.encoding)
+        self.file = None
         self.man = []
         self.score = []
         self.woman = []
         self.subject = None
+        self.year = 0
 
     def process(self):
+        current_directory = os.getcwd()
+        file_list = os.listdir(current_directory)
+        csv_list = [f for f in file_list if f.endswith('.csv')]
+        year = [csv[:4] for csv in csv_list]
+        print("\n".join(year))
+        while True:
+            self.year = input('연도를 입력하세요 >')
+            if self.year in year:
+                break
+            else:
+                print('존재하지 않는 연도입니다')
+        for file in csv_list:
+            if file.startswith(self.year):
+                self.path = file
+        self.file = pd.read_csv(self.path, encoding=self.encoding)
         subjects = self.file['유형'].unique()
         subjects_dict = {i: value for i, value in enumerate(subjects, start=1)}
         for i, value in enumerate(subjects, start=1):
