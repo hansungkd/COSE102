7b5c8e363073d8fabd87a2213e407356237285e8
hansung-ku <hansungk@korea.ac.kr>
2024-12-06 22:33:49 +0900

    그래프를 누적 그래프로 변경

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from fileProcess import FileProcess
 import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_graph(subject, score, man, woman):
+def plot_graph(year, subject, score, man, woman):
     plt.rcParams['font.family'] = 'NanumGothic'
 
-    plt.plot(score, man, label='남', linestyle='-', linewidth=2, alpha=0.7)
-    plt.scatter(score, man, alpha=0.7, s=15)
+    man_cumulative = np.cumsum(man[::-1])[::-1]
+    woman_cumulative = np.cumsum(woman[::-1])[::-1]
 
-    plt.plot(score, woman, label='여', linestyle='-', linewidth=2, alpha=0.7)
-    plt.scatter(score, woman, alpha=0.7, s=15)
+    plt.fill_between(score, 0, man_cumulative, label='남', alpha=0.5)
+    plt.fill_between(score, 0, woman_cumulative, label='여', alpha=0.5)
 
-    plt.title(f"2024학년도 수능 {subject} 과목 성적 분포")
+    plt.title(f"{int(year) + 1}학년도 수능 {subject} 과목 성적 분포")
     plt.xlabel("점수")
-    plt.ylabel("인원수")
+    plt.ylabel("누적 인원수")
     plt.legend()
+
+    plt.tight_layout()
 
     plt.show()
 
 if __name__ == '__main__':
-    path = '20231231.csv'
-    file = FileProcess(path)
+    file = FileProcess()
     file.process()
-    plot_graph(file.subject, file.score, file.man, file.woman)
+    plot_graph(file.year, file.subject, file.score, file.man, file.woman)
 
 
